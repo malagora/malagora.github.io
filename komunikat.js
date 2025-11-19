d5ff6ff7706a5b70fe5d663e5302d37e23e3395d
Mała Góra <malagora.weryfikacja@vp.pl>
2025-11-19 08:57:22 +0100

    Update komunikat.js

--- komunikat.js
+++ komunikat.js
@@ -1,5 +1,5 @@
 // === WYBÓR TEKSTU ===
-const selectedText = 3; // 1-słońce 2-wiatr 3-deszcz 4-śnieg 5-błoto 6-deszcz&wiatr 7-śnieżyca 8-błoto&wiatr
+const selectedText = 10; // 1-słońce 2-wiatr 3-deszcz 4-śnieg 5-błoto 6-deszcz&wiatr 7-śnieżyca 8-błoto&wiatr 9-lód 10-lód&słońce
 
 const moreTxt = "<br>Wezbrana woda porwała kładkę do parku. Przejście tym sposobem NIE JEST MOŻLIWE!<br>"; //<br>...<br>
 
@@ -40,6 +40,8 @@ const texts = [
   "Na szlakach mokro i ślisko. Wieje silny wiatr.",
   "Intensywne opady śniegu.",
   "Na szlakach zalega błoto. Wieje silny wiatr.",
+  "Szlaki są oblodzone.",
+  "Szlaki są oblodzone, jednak świeci słońce."
 ];
 
 const komunikaty = [
@@ -51,6 +53,9 @@ const komunikaty = [
   "Na szlakach jest mokro i ślisko. W skutek opadu deszczu tworzy się błoto, które w eksponowanym terenie może prowadzić do upadku z wysokości.<br>Dodatkowo występują silne podmuchy wiatru, które mogą powodować łamanie się gałęzi.<br>Poruszanie się w takich warunkach wymaga zachowania ostrożności.",
   "Intensywne opady śniegu. Nie wychodź na szlak podczas śnieżycy!<br>Warunki są bardzo wymagające, zaś długotrwała ekspozycja na zimno może powodować utratę świadomości. W takich warunkach lepiej pozostać na kwaterze.<br>Dodatkowo występują silne podmuchy wiatru, które mogą powodować łamanie się gałęzi.<br>Poruszanie się w takich warunkach wymaga stosowania szczególnej ostrożności. Zachęcamy do zabrania ze sobą odpowiedzniego sprzętu, jednakże <b style='font-weight: 600;'>ODRADZAMY wyjście w takich warunkach!</b>",
   "Po opadach deszczu na szlakach zalega błoto, które w eksponowanym terenie może spowodować upadek z wysokości.<br>Dodatkowo występują silne podmuchy wiatru, które mogą powodować łamanie się gałęzi.<br>Poruszanie się w takich warunkach wymaga zachowania ostrożności.",
+  "Ujemne temperatury sprawiły, że podłoże jest oblodzone. Pogoda nie sprzyja pieszym wędrówkom
+<br>Poruszanie się w takich warunkach wymaga szczególnej ostrożności oraz stosowania odpowiedniego sprzętu (raczki, raki, czekan).",
+  "Error 400 – brak treści"
 ];
 
 const standardTxt = "<br>Poruszając się po szlakach stosuj się do niżej wymienionych zasad i zaleceń.<br>";
